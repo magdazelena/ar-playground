2a862b18006f4a276186a4258d66720e44ec77f9
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2023-05-15 14:55:27 +0200

    added cursor

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
-console.log("hey");
+function onSceneLoaded() {
+  const raycaster = document.querySelector("[ar-raycaster]");
+  const cursor = document.querySelector("#cursor");
+  raycaster.addEventListener("raycaster-intersection", (event) => {
+    console.log(event);
+    cursor.setAttribute("position", event.detail.intersections[0].point);
+  });
+}
+document.addEventListener("DOMContentLoaded", function (event) {
+  const scene = document.querySelector("a-scene");
+  scene.addEventListener("loaded", onSceneLoaded);
+});
+function loadPosition(e) {
+  console.log(e);
+}
