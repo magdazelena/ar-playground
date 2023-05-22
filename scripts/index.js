6d7f0124ea3c8926b110d7cfc65653a230ed494c
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2023-05-22 15:34:49 +0200

    wip
    wip
    
    wip
    
    wip
    
    wip
    
    test
    
    test
    
    test
    
    test

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,30 @@ function onSceneLoaded() {
   const raycaster = document.querySelector("[ar-raycaster]");
   const cursor = document.querySelector("#cursor");
   const obj = document.querySelector("#obj");
+  const rig = document.querySelector("#rig");
+  let firstTime = true;
+  let count = 0;
   raycaster.addEventListener("raycaster-intersection", (event) => {
-    console.log(event.target.components);
+    count += 1;
+    if (firstTime) return (firstTime = false);
+    alert("hey" + count);
+    const rigCamera = rig.querySelector("#focus-camera");
+    rigCamera.setAttribute("camera", "active", true);
   });
 
-  let firstTime = true;
-  obj.addEventListener("click", () => {
-    console.log(this.camera.el.object3D);
-    if (firstTime) {
-      const pos = this.camera.el.object3D.getWorldPosition();
-      obj.setAttribute("position", pos);
-      firstTime = false;
-    }
-  });
+  // let firstTime = true;
+  // obj.addEventListener("click", () => {
+  //   console.log(this.camera.el.object3D);
+  //   if (firstTime) {
+  //     const pos = this.camera.el.object3D.getWorldPosition();
+  //     obj.setAttribute("position", pos);
+  //     firstTime = false;
+  //   }
+  // });
 }
 document.addEventListener("DOMContentLoaded", function (event) {
   const scene = document.querySelector("a-scene");
-  scene.addEventListener("loaded", onSceneLoaded);
+  //scene.addEventListener("loaded", onSceneLoaded);
   if (window.DeviceOrientationEvent) {
     // Listen for device orientation changes
     window.addEventListener("deviceorientation", handleDeviceOrientation, true);
@@ -33,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var gamma = event.gamma; // Y-axis rotation
 
     // Update camera rotation based on device rotation
-    var cameraEl = document.querySelector("a-entity[camera]");
+    var cameraEl = document.querySelector("#rig");
     cameraEl.setAttribute("rotation", {
       x: beta,
       y: gamma,
