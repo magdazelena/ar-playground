002f27272febde808456d0fc20ac48571d7ba7b1
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2023-05-24 17:52:13 +0200

    adjust rotations, setup acc

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,11 @@ window.addEventListener("DOMContentLoaded", function () {
   AFRAME.registerComponent("first-person-controls", {
     init: function () {
       this.cameraRigEl = document.querySelector("#rig");
-      this.camera = document.querySelector("#camera");
+      var scene = document.querySelector("a-scene").object3D;
+      this.camera = scene.children.find(
+        (el) => el.type === "PerspectiveCamera"
+      );
+
       // Set initial position of the camera rig based on the camera's current position
       this.cameraRigEl.object3D.position.copy(this.camera.object3D.position);
 
@@ -14,12 +18,12 @@ window.addEventListener("DOMContentLoaded", function () {
       this.cameraRigEl.object3D.rotation.copy(this.camera.object3D.rotation);
       this.controls = new FirstPersonControls(
         this.camera.object3D,
-        this.camera.parentElement
+        this.cameraRigEl
       );
-      this.controls.lookSpeed = 0.5;
+      this.controls.lookSpeed = 0.1;
       this.controls.movementSpeed = 10;
       this.controls.noFly = true;
-      this.controls.lookVertical = true;
+      this.controls.lookVertical = false;
     },
     tick: function (time, delta) {
       this.cameraRigEl.object3D.position.copy(this.camera.object3D.position);
@@ -32,14 +36,17 @@ window.addEventListener("DOMContentLoaded", function () {
   });
   AFRAME.registerComponent("scene-setup", {
     init: function () {
-      this.objectEl = document.querySelector("#object");
-      this.cameraRigEl = document.querySelector("#rig");
+      var cameraRigEl = document.querySelector("#rig");
+      var objectEl = document.querySelector("#object");
 
       // Set initial camera position
-      this.objectEl.object3D.position.set(0, 0, 0);
+      cameraRigEl.object3D.position.set(0, 0, 0);
+
+      // Set initial object position
+      objectEl.object3D.position.copy(cameraRigEl.object3D.position);
 
       // Add FirstPersonControls to cameraRig
-      this.cameraRigEl.setAttribute("first-person-controls", "");
+      cameraRigEl.setAttribute("first-person-controls", "");
     },
   });
   if (sceneEl.hasLoaded) {
@@ -47,9 +54,28 @@ window.addEventListener("DOMContentLoaded", function () {
   } else {
     sceneEl.addEventListener("loaded", run);
   }
+  if (window.DeviceMotionEvent) {
+    window.addEventListener("devicemotion", motion, false);
+  } else {
+    console.log("DeviceMotionEvent is not supported");
+  }
   function run() {
     var setupEl = document.createElement("a-entity");
     setupEl.setAttribute("scene-setup", "");
     sceneEl.appendChild(setupEl);
   }
 });
+
+function motion(event) {
+  const helper = document.querySelector("#helpers");
+  const downTilt = event.accelerationIncludingGravity.y; //10 - vertical (en face) 0 - horizontal (down)
+  const upTilt = event.accelerationIncludingGravity.z; //0 - vertical (en face) -10 - horizontal (up)
+  console.log(event);
+  helper.textContent =
+    "Accelerometer: x: " +
+    Math.round(event.accelerationIncludingGravity.x) +
+    ", y: " +
+    Math.round(event.accelerationIncludingGravity.y) +
+    ", z: " +
+    Math.round(event.accelerationIncludingGravity.z);
+}
