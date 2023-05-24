9da04ceec565feef75459711d8086089634f35f3
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2023-05-24 19:55:42 +0200

    chnaged model, adjusted scaling

--- scripts/index.js
+++ scripts/index.js
@@ -6,32 +6,64 @@ window.addEventListener("DOMContentLoaded", function () {
   AFRAME.registerComponent("first-person-controls", {
     init: function () {
       this.cameraRigEl = document.querySelector("#rig");
-      var scene = document.querySelector("a-scene").object3D;
-      this.camera = scene.children.find(
-        (el) => el.type === "PerspectiveCamera"
-      );
-
+      this.cameraEl = document.querySelector("#camera");
+      this.camera = this.cameraEl.object3D.children[0];
+      this.distance = 1.5;
+      this.scaleFactor = 1.0;
       // Set initial position of the camera rig based on the camera's current position
-      this.cameraRigEl.object3D.position.copy(this.camera.object3D.position);
+      this.cameraRigEl.object3D.position.copy(this.camera.position);
 
       // Set the initial rotation of the camera rig based on the camera's current rotation
-      this.cameraRigEl.object3D.rotation.copy(this.camera.object3D.rotation);
-      this.controls = new FirstPersonControls(
-        this.camera.object3D,
-        this.cameraRigEl
-      );
+      this.cameraRigEl.object3D.rotation.copy(this.camera.rotation);
+
+      // Create a helper object to represent the desired distance from the camera to the object
+      this.helperObject = new THREE.Object3D();
+      this.helperObject.position.z = -this.distance;
+      this.camera.add(this.helperObject);
+
+      this.controls = new FirstPersonControls(this.camera, this.cameraEl);
       this.controls.lookSpeed = 0.1;
       this.controls.movementSpeed = 10;
       this.controls.noFly = true;
-      this.controls.lookVertical = false;
+      this.controls.lookVertical = true;
+      window.addEventListener(
+        "devicemotion",
+        this.handleDeviceMotion.bind(this)
+      );
     },
     tick: function (time, delta) {
-      this.cameraRigEl.object3D.position.copy(this.camera.object3D.position);
+      this.cameraRigEl.object3D.position.copy(this.camera.position);
 
       // Update the rotation of the camera rig based on the camera's current rotation
-      this.cameraRigEl.object3D.rotation.copy(this.camera.object3D.rotation);
+      this.cameraRigEl.object3D.rotation.copy(this.camera.rotation);
+
+      var position = new THREE.Vector3(0, 0, -this.distance);
+      position.applyQuaternion(this.camera.quaternion);
+      position.add(this.camera.position);
+
+      // Update the position of the object in the world coordinates
+      var objectEl = document.querySelector("#object");
+      objectEl.object3D.position.copy(position);
+      objectEl.object3D.scale.set(
+        this.scaleFactor,
+        this.scaleFactor,
+        this.scaleFactor
+      );
 
       this.controls.update(delta / 1000);
+    },
+    handleDeviceMotion: function (event) {
+      var acceleration = event.accelerationIncludingGravity;
+      var accelerationX = acceleration.x || 0;
+      var accelerationY = acceleration.y || 0;
+      var accelerationZ = acceleration.z || 0;
+
+      // Adjust distance based on acceleration
+      this.distance -= accelerationZ * 0.1; // Adjust the sensitivity as needed
+
+      // Clamp the distance within a specific range
+      this.distance = Math.max(0.5, Math.min(5, this.distance));
+      this.scaleFactor = Math.max(1.0, Math.min(0.1, 1.0 / this.distance));
     },
   });
   AFRAME.registerComponent("scene-setup", {
@@ -70,7 +102,6 @@ function motion(event) {
   const helper = document.querySelector("#helpers");
   const downTilt = event.accelerationIncludingGravity.y; //10 - vertical (en face) 0 - horizontal (down)
   const upTilt = event.accelerationIncludingGravity.z; //0 - vertical (en face) -10 - horizontal (up)
-  console.log(event);
   helper.textContent =
     "Accelerometer: x: " +
     Math.round(event.accelerationIncludingGravity.x) +
